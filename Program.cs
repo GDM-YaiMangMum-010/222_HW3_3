f2f96560579c5b4cc97588a06fef142604bef017
Jirayu_SBTR <jirayu.majesty@mail.kmutt.ac.th>
2023-03-28 23:52:18 +0700

    Patch 0.3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        double c1x,c1y,c2x,c2y,c1r,c2r;
+        double c1x,c1y,c2x,c2y,c1r,c2r,a1x,a1y;
         c1x = double.Parse(Console.ReadLine());
         c1y = double.Parse(Console.ReadLine());
         c2x = double.Parse(Console.ReadLine());
@@ -30,7 +30,21 @@ class Program
         }
         else
         {
-            
+            double dx = c1x - c2x;
+            double dy = c1y - c2y;
+            double d = Math.Sqrt(dx*dx + dy*dy);
+            double gamma1 = Math.Acos((c2r*c2r + d*d - c1r*c1r)/(2*c2r*d));
+            double d1 = c1r*Math.Cos(gamma1);
+            double h = c1r*Math.Sin(gamma1);
+            double px = c1x + (c2x - c1x) / d*d1;
+            double py = c1y + (c2y - c1y) / d*d1;
+            Vector2 intersec1 = new Vector2(px + (-dy) / d * h, py + (+dx) / d * h);
+            Vector2 intersec2 = new Vector2(px - (-dy) / d * h, py - (+dx) / d * h);
+            Console.WriteLine(intersec1.x);
+            Console.WriteLine(intersec1.y);
+            Console.WriteLine(intersec2.x);
+            Console.WriteLine(intersec2.y);
         }
     }
+
 }
