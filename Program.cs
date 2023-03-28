2c2f18f8bb8acb13ba8a0328a4aadf67ee85608a
Jirayu_SBTR <jirayu.majesty@mail.kmutt.ac.th>
2023-03-28 23:31:12 +0700

    Input0.1

--- Program.cs
+++ Program.cs
@@ -4,45 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Vector2 one = new Vector2(double.Parse(Console.ReadLine()),double.Parse(Console.ReadLine()));
-        Vector2 two = new Vector2(double.Parse(Console.ReadLine()),double.Parse(Console.ReadLine()));
+        double c1x,c1y,c2x,c2y,c1r,c2r;
+        c1x = double.Parse(Console.ReadLine());
+        c1y = double.Parse(Console.ReadLine());
+        c2x = double.Parse(Console.ReadLine());
+        c2y = double.Parse(Console.ReadLine());
+        c1r = double.Parse(Console.ReadLine());
+        c2r = double.Parse(Console.ReadLine());
 
-        Circle2 Cone = new Circle2(one,double.Parse(Console.ReadLine()));
-        Circle2 Ctwo = new Circle2(two,double.Parse(Console.ReadLine()));
+        Vector2 v_c1 = new Vector2(c1x,c1y);
+        Vector2 v_c2 = new Vector2(c2x,c2y);
 
-        IntersectCircles(one.x,one.y,Cone.r,two.x,two.y,Ctwo.r);
+        Circle2 c_c1 = new Circle2(v_c1,c1r);
+        Circle2 c_c2 = new Circle2(v_c2,c2r);
 
-    }
-
-
-    public static void IntersectCircles(double x1, double y1, double r1, double x2, double y2, double r2)
-    {
-        double dx = x2 - x1;
-        double dy = y2 - y1;
-        double d = Math.Sqrt(dx*dx + dy*dy);
-
-        if (d > r1 + r2 || d < Math.Abs(r1 - r2))
+        double magnitude = Math.Sqrt((v_c2.x-v_c1.x) * (v_c2.x-v_c1.x) + (v_c2.y-v_c1.y) * (v_c2.y-v_c1.y));
+        double sum_r = c1r+c2r;
+        if(magnitude > sum_r)
         {
-            return ;
         }
-
-        double a = (r1*r1 - r2*r2 + d*d) / (2*d);
-        double h = Math.Sqrt(r1*r1 - a*a);
-        double cx2 = x1 + a*dx/d;
-        double cy2 = y1 + a*dy/d;
-        double ix1 = cx2 + h*dy/d;
-        double iy1 = cy2 - h*dx/d;
-        double ix2 = cx2 - h*dy/d;
-        double iy2 = cy2 + h*dx/d;
-
-        Console.WriteLine("{0:0.00}",ix2);
-        Console.WriteLine("{0:0.00}",iy2);
-        
-        if(ix2!=ix1&&iy2!=iy1){
-            Console.WriteLine("{0:0.00}",ix1);
-            Console.WriteLine("{0:0.00}",iy1);
+        else
+        {
+            
         }
-        return ;
     }
-
 }
